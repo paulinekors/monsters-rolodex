fabb99f797f0ea52fe9a8b60300647b543d21b5a
paulinekors <paulinekors@gmail.com>
2020-02-29 11:39:51 +0100

    Updates

---
+++ precache-manifest.c07cd1d446cf505b9c3bb98333f873de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78bcc6359ed50654c49f67587c238ffe",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e1282488651c09f89d96",
+    "url": "/monsters-rolodex/static/css/main.4e398d8d.chunk.css"
+  },
+  {
+    "revision": "ebea2ca50f5ac2a2ca9a",
+    "url": "/monsters-rolodex/static/js/2.ff87c9fa.chunk.js"
+  },
+  {
+    "revision": "e1282488651c09f89d96",
+    "url": "/monsters-rolodex/static/js/main.15573517.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
